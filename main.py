1dd6d3d570028cd12114dec6135f4930a1e505b4
Debdal Chowdhury <53616883+debdal6@users.noreply.github.com>
2024-10-17 23:23:12 +0100

    troubleshooting-II

--- main.py
+++ main.py
@@ -3,28 +3,32 @@ import matplotlib.pyplot as pyPlot
 
 # Global Constants
 
-lengthOfRod = 100 #n
-unitTime = 10 #m
-heatConductivity = 385
-xVectorStepSize = 1 #h
-yVectorStepSize = 1 #k
+lengthOfRod = 10 #n
+unitTime = 1 #m
+heatConductivity = 100
+xVectorStepSize = 0.25 #h
+yVectorStepSize = 0.025 #k
 
 # Length Vector plotted on x-axis
 xVector = numpy.arange(0, lengthOfRod+xVectorStepSize, xVectorStepSize)
 # Time Vector plotted on y-axis
 yVector = numpy.arange(0, unitTime+yVectorStepSize, yVectorStepSize)
 
-boundaryConditions = [1084, 1084]
-intialConditions = numpy.sin(numpy.pi*xVector)
+boundaryConditions = [numpy.sin(lengthOfRod)+numpy.sin(unitTime), numpy.sin(lengthOfRod)]
+intialConditions = numpy.sin(xVector)
+
+xVectorLength = len(xVector)
+yVectorLength = len(yVector)
+
 
 # Empty Matrix/NestedList with zeroes
-gridMatrix = numpy.zeros((lengthOfRod, unitTime))
+gridMatrix = numpy.zeros((xVectorLength, yVectorLength))
 gridMatrix[0,:] = boundaryConditions[0]
 gridMatrix[-1,:] = boundaryConditions[1]
 gridMatrix[:, 0] = intialConditions
 
-for deltaTime in range (1, unitTime-1):
-    for deltaLength in range (1, lengthOfRod-1):
+for deltaTime in range (1, yVectorLength-1):
+    for deltaLength in range (1, xVectorLength-1):
         gridMatrix[deltaLength,deltaTime]=(
             (
                 (heatConductivity*deltaTime-1)/deltaLength**2)*
@@ -33,7 +37,7 @@ for deltaTime in range (1, unitTime-1):
                  gridMatrix[deltaLength+1,deltaTime-1])
             ) + gridMatrix[deltaLength,deltaTime-1]
 
-print(gridMatrix)
+gridMatrix=gridMatrix
 
 pyPlot.plot(gridMatrix)
 pyPlot.show()
