fbd08307cb77f80ad6ec9be5e5f3a62981d7c771
Debdal Chowdhury <53616883+debdal6@users.noreply.github.com>
2024-10-17 23:03:00 +0100

    troubleshooting

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ unitTime = 10 #m
 heatConductivity = 385
 xVectorStepSize = 1 #h
 yVectorStepSize = 1 #k
-boundaryConditions = [1084, 1084]
-intialConditions = 24
+
 # Length Vector plotted on x-axis
-# xVector = numpy.arange(0, lengthOfRod+xVectorStepSize, xVectorStepSize)
+xVector = numpy.arange(0, lengthOfRod+xVectorStepSize, xVectorStepSize)
 # Time Vector plotted on y-axis
-# yVector = numpy.arange(0, unitTime+yVectorStepSize, yVectorStepSize)
+yVector = numpy.arange(0, unitTime+yVectorStepSize, yVectorStepSize)
+
+boundaryConditions = [1084, 1084]
+intialConditions = numpy.sin(numpy.pi*xVector)
 
 # Empty Matrix/NestedList with zeroes
 gridMatrix = numpy.zeros((lengthOfRod, unitTime))
@@ -23,13 +25,13 @@ gridMatrix[:, 0] = intialConditions
 
 for deltaTime in range (1, unitTime-1):
     for deltaLength in range (1, lengthOfRod-1):
-        gridMatrix[deltaLength,deltaTime+1]=(
+        gridMatrix[deltaLength,deltaTime]=(
             (
-                (heatConductivity*deltaTime)/deltaLength**2)*
-                (gridMatrix[deltaLength-1,deltaTime]-
-                 2*gridMatrix[deltaLength,deltaTime]+
-                 gridMatrix[deltaLength+1,deltaTime])
-            ) + gridMatrix[deltaLength,deltaTime]
+                (heatConductivity*deltaTime-1)/deltaLength**2)*
+                (gridMatrix[deltaLength-1,deltaTime-1]-
+                 2*gridMatrix[deltaLength,deltaTime-1]+
+                 gridMatrix[deltaLength+1,deltaTime-1])
+            ) + gridMatrix[deltaLength,deltaTime-1]
 
 print(gridMatrix)
 
